cd95cc6e72c201aa5239f43e624417232f583987
youssefbourais0310 <youssef.bourais0310@gmail.com>
2023-05-24 14:41:27 +0100

    returning the pointer to the valid line command, handling double and single quote

--- minishell.c
+++ minishell.c
@@ -6,17 +6,13 @@
 /*   By: ybourais <ybourais@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 10:59:39 by ybourais          #+#    #+#             */
-/*   Updated: 2023/05/23 15:48:56 by ybourais         ###   ########.fr       */
+/*   Updated: 2023/05/24 13:14:17 by ybourais         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 
-// void exucution(char *input)
-// {
-//     system(input);
-// }
 
 int main(int argc, char **argv)
 {
@@ -24,41 +20,13 @@ int main(int argc, char **argv)
     (void)argv;
     
     char *input;
-    // struct stat s;
     
     while(1)
     {
         input = readline("\e[1;32mmy_Shell-310$ \e[0m");
-
-        // if (check_quoting(input) == 1)
-        //     printf("true\n");
-        // else if (check_quoting(input) == 0)
-        //     printf("false\n");
-            
-        // parssing(input);
-        // if (check_quoting(input) == 0)
-        //     printf("false\n");
-        // else
-        //     printf("true\n");
+        add_history(input);
         check_quoting(input);
+        printf("%s\n", input);
     }
-    // DIR *d;
-    // struct dirent *dd;
-
-    // d = opendir("/Users/ybourais/Desktop/minishell");
-
-    // while ((dd = readdir(d)) != NULL)
-    //     printf("%hhu\n", dd->d_name);
-
-    // closedir(d);
-    // char *tab[] = {"/bin/ls", "-a", NULL};
-    // execve(tab[0], tab, NULL);
-    // unlink("/Users/ybourais/Desktop/minishell/yichiba");
-    // stat("/Users/ybourais/Desktop/minishell/note.txt", &s);
-    // printf("%lld\n", s.st_size);
-    //chdir("..");
-    // printf("%s\n", getcwd(input, 250));
-    // printf("%d\n", access("note.txt", W_OK | R_OK));
     return 0;
 }
-
--- parssing.c
+++ parssing.c
@@ -6,7 +6,7 @@
 /*   By: ybourais <ybourais@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 10:05:20 by ybourais          #+#    #+#             */
-/*   Updated: 2023/05/23 20:12:39 by ybourais         ###   ########.fr       */
+/*   Updated: 2023/05/24 14:38:33 by ybourais         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,47 +32,43 @@ int inside_quot(char *str, int i, int p, char *token)
     {
         token[i] = str[i];
         i++;
-        // printf("%c", str[i++]);
     }
     return i;
+}
+
+void ft_error(int n)
+{
+    if(n == 1)
+    {
+        write(n, "Error\n", 6);
+        exit(0);
+    }
 }
 
 void check_quoting(char *str)
 {
     int i;
     int j = 0;
-    char token[100];
-    
+    int quote;
+
     i = 0;
     while (str[i] != '\0')
     {
         if (str[i] != 34 && str[i] != 39)
-            token[j] = str[i];
-        else if(str[i] == 34)
+            str[j] = str[i];
+        else if(str[i] == 34 || str[i] == 39)
         {
-            i = i + 1;
-            while (str[i] != 34)
-                token[j++] = str[i++];
-            j--;
-        }
-        else if(str[i] == 39)
-        {
-            i = i + 1;
-            while (str[i] != 39)
-                token[j++] = str[i++];           
+            quote = str[i++];
+            while (str[i] != quote)
+            {
+                if(str[i] == '\0')
+                    ft_error(1);
+                str[j++] = str[i++];
+            }
             j--;
         }
         i ++;    
-        j ++;    
+        j ++;
     }
-    token[j] = '\0';
-    printf("%s\n", token);
-    
+    str[j] = '\0';
 }
-
-// i = inside_quot(str, i + 1, 34, token); /* double quot */
-// i = inside_quot(str, i + 1, 39, token); /* single quot */
-// void parssing(char *str)
-// {
-
-// }
